f17e0ab1b210e56fb2710d5bcf6bdd5224a47cdf
mariafernandacll2808 <124931004+mariafernandacll2808@users.noreply.github.com>
2023-04-05 23:24:00 -0500

    Clases Producto y Tienda

---
+++ src/gestorAplicacion/produccion/Producto.java
@@ -0,0 +1,69 @@
+package gestorAplicacion.produccion;
+import gestorAplicacion.gestion.Factura;
+
+public class Producto {
+    
+    //Atributos
+    private String descripcion;
+    private Double valor;
+    private Double peso;
+    private Double tamano;
+    private Factura factura;
+
+    //Constructor 
+    public Producto(String descripcion, Double valor, Double peso, Double tamano, Factura factura) {
+        this.descripcion = descripcion;
+        this.valor = valor;
+        this.peso = peso;
+        this.tamano = tamano;
+        this.factura = factura;
+    }
+
+    //Métodos
+
+
+    //Get and Set
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getTamano() {
+        return this.tamano;
+    }
+
+    public void setTamano(Double tamano) {
+        this.tamano = tamano;
+    }
+
+    public Factura getFactura() {
+        return this.factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+
+
+    
+}
---
+++ src/gestorAplicacion/produccion/Tienda.java
@@ -0,0 +1,80 @@
+package gestorAplicacion.produccion;
+import gestorAplicacion.gestion.CuentaBancaria;
+import gestorAplicacion.gestion.Vendedor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Tienda {
+
+    //Atributos
+    private String nombre;
+    private Vendedor vendedor;
+    private CuentaBancaria cuenta;
+    private ArrayList<Producto> listaProductos; 
+    private Map<Producto, Integer> listaCantidadProductos; 
+    
+    //Constructor que recibe solo nombre, vendedor y cuenta 
+    public Tienda(String nombre,Vendedor vendedor,CuentaBancaria cuenta){
+        this.nombre = nombre;
+        this.vendedor = vendedor;
+        this.cuenta = cuenta;
+        this.listaProductos = new ArrayList<Producto>();
+        this.listaCantidadProductos = new HashMap<Producto, Integer>();
+    }
+
+    //Constructor que recibe todos los parametros
+    public Tienda(String nombre, Vendedor vendedor, CuentaBancaria cuenta, ArrayList<Producto> listaProductos, Map<Producto,Integer> listaCantidadProductos) {
+        this.nombre = nombre;
+        this.vendedor = vendedor;
+        this.cuenta = cuenta;
+        this.listaProductos = listaProductos;
+        this.listaCantidadProductos = listaCantidadProductos;
+    }
+    
+    //Métodos
+
+    
+    //Get and Set
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Vendedor getVendedor() {
+        return this.vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public CuentaBancaria getCuenta() {
+        return this.cuenta;
+    }
+
+    public void setCuenta(CuentaBancaria cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return this.listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public Map<Producto,Integer> getListaCantidadProductos() {
+        return this.listaCantidadProductos;
+    }
+
+    public void setListaCantidadProductos(Map<Producto,Integer> listaCantidadProductos) {
+        this.listaCantidadProductos = listaCantidadProductos;
+    }
+
+}
