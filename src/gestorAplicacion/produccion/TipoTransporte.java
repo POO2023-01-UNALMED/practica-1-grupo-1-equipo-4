e3eae2adb79278767e59cefcfcc655675dd5063b
MSRestreLeon <monicarestrepoleon@gmail.com>
2023-04-06 20:21:24 -0500

    Cambios a TipoTransporte

--- src/gestorAplicacion/produccion/TipoTransporte.java
+++ src/gestorAplicacion/produccion/TipoTransporte.java
@@ -3,23 +3,23 @@ package gestorAplicacion.produccion;
 public enum TipoTransporte {
     
     //los tipos de transporte que hay 
-    Camion(15000, 500),
-    Avion(30000, 10000),
-    Carro(10000,600),
-    Automovil(9000,400),
-    Camioneta(12000, 700), 
-    Bicicleta(5000,100),
-    Patines(3000,50),
-    Barco(20000, 400),
-    Caminando(5000, 20); 
+    Camion(15000, 16329),
+    Avion(30000, 640000),
+    Automovil(9000,500),
+    Camioneta(12000, 650), 
+    Bicicleta(5000,35),
+    Patines(3000,20),
+    Barco(20000, 33565835),
+    Container(20000, 29000),
+    Caminando(5000, 15); 
 
     //cuanto vale cada uno 
-    private double precioEnvio; //dado en pesos
-    private double distanciaMax; //dado en kilometros recorridos en un día
-
-    private TipoTransporte(double precioEnvio, double distanciaMax) {
-        precioEnvio = precioEnvio;
-        distanciaMax = distanciaMax;
+    public double precioEnvio; //dado en pesos
+    public double capacidadMax; //dado en kilogramos
+    
+    private TipoTransporte(double precioEnvio, double capacidadMax) {
+        this.precioEnvio = precioEnvio;
+        this.capacidadMax = capacidadMax;
     }
 
 
