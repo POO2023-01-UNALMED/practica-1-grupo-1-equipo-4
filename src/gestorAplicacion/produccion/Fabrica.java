d64264dae7bf99e2e9f08cd00ac995cbf0f366c8
Wakamole2002 <luvarelao@unal.edu.co>
2023-04-19 22:42:49 -0500

    commit Fabrica
    pase todo los objetos y cosas que creaba en fabrica al main y lo comente, tambien se creo el metodo mostrar y seleccionar tienda.

--- src/gestorAplicacion/produccion/Fabrica.java
+++ src/gestorAplicacion/produccion/Fabrica.java
@@ -1,5 +1,7 @@
 package gestorAplicacion.produccion;
 import gestorAplicacion.gestion.CuentaBancaria;
+import gestorAplicacion.produccion.Tienda;
+import gestorAplicacion.produccion.Producto;
 import gestorAplicacion.gestion.Financiero;
 import gestorAplicacion.gestion.Operario;
 import gestorAplicacion.gestion.Vendedor;
@@ -10,94 +12,19 @@ public class Fabrica implements Financiero{
 
     private static ArrayList<Operario> listaOperadores;
     private static ArrayList<Producto> listaProductos;
-    private static int cantidadProducto;
+    private static int cantidadProducto; //se supone que esta es la cantidad a repartir y va a ser fija, puede quitarse el atributo o no xd (consultar)
     private static ArrayList<Tienda> listaTienda;
     private static CuentaBancaria CuentaBancaria;
 
 
     // ------------- constructor -------------
-    public Fabrica() {
-
-         //NOTA: AÚN NO SE SI ESTO VA ACA O EN UN METODO ESTATICO 
-
-            //productos disponibles en las fabricas:
-
-            ArrayList<Producto> catalogo = new ArrayList<Producto>();
-            //Producto(String nombre, String descripcion, Double valor, Double peso, Double tamano, Double costoDeProduccion);
-            Producto producto1 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto2 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto3 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto4 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto5 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto6 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto7 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto8 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto9 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            Producto producto10 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
-            //se agregan al catalogo
-            catalogo.add(producto1);
-            catalogo.add(producto2);
-            catalogo.add(producto3);
-            catalogo.add(producto4);
-            catalogo.add(producto5);
-        
-            //empleados de la fabrica:
-            
-            //cuentas para los empleados:
-            CuentaBancaria cuentaOperario1 = new CuentaBancaria(55555, 100000);
-            CuentaBancaria cuentaOperario2 = new CuentaBancaria(66666, 100000);
-            CuentaBancaria cuentaOperario3 = new CuentaBancaria(77777, 100000);
-        
-            ArrayList<Operario> nomina = new ArrayList<Operario>();
-            Operario operario1 = new Operario("operador1",20,55555,cuentaOperario1,Fabrica, 0);//cada oeprador tiene cien mil *hay que sacar cuentas*
-            Operario operario2 = new Operario("operador2",21,66666,cuentaOperario2,Fabrica, 0);
-            Operario operario3 = new Operario("operador3",22,77777,cuentaOperario3,Fabrica, 0);
-            //se agg a la nomina
-            nomina.add(operario1);
-            nomina.add(operario2);
-            nomina.add(operario3);
-        
-            //tiendas disponibles
-
-            //se crean los empleados para las tiendas:
-            //cuentas para los empleados de la tienda:
-            CuentaBancaria cuentaVendedor1 = new CuentaBancaria(55555, 100000); //cada vendedor tiene cien mil *hay que sacar cuentas*
-            CuentaBancaria cuentaVendedor2 = new CuentaBancaria(66666, 100000);
-            CuentaBancaria cuentaVendedor3 = new CuentaBancaria(77777, 100000);
-            //vendedores
-            Vendedor vendedor1 = new Vendedor("vendedor1",20,55555,cuentaVendedor1,Fabrica, 0);
-            Vendedor vendedor2 = new Vendedor("vendedor2",21,66666,cuentaVendedor2,Fabrica, 0);
-            Vendedor vendedor3 = new Vendedor("vendedor3",22,77777,cuentaVendedor3,Fabrica, 0);
-
-            //tiendas
-            ArrayList<Tienda> tiendas = new ArrayList<Tienda>();
-            Tienda tienda1 = new Tienda("tienda1",vendedor1,cuentaVendedor1);
-            Tienda tienda2 = new Tienda("tienda2",vendedor2,cuentaVendedor2);
-            Tienda tienda3 = new Tienda("tienda3",vendedor3,cuentaVendedor3);
-        
-            tiendas.add(tienda1);
-            tiendas.add(tienda2);
-            tiendas.add(tienda3);
-
-            //se le asigna la tienda a cada vendedor:
-            vendedor1.setTienda(tienda1);
-            vendedor2.setTienda(tienda2);
-            vendedor3.setTienda(tienda3);
-        //cuenta bancaria para la fabrica:
-        CuentaBancaria cuentaFabrica = new CuentaBancaria(00000, 1000000); //saldo = un millon
-
-        //asignacion a los atributos de la clase:
-
-        Fabrica.listaOperadores = nomina;
-        Fabrica.listaProductos = catalogo;
-        Fabrica.listaTienda = tiendas;
-        CuentaBancaria = cuentaFabrica;
-        cantidadProducto = 10;
-
-        //unimos la fabrica creada con sus respectivosb
-        operario1.setFabrica(this);
-        operario2.setFabrica(this);
-        operario3.setFabrica(this);
+    public Fabrica(ArrayList<Operario> listaOperadores, ArrayList<Producto> listaProductos, int cantidadProducto,
+            ArrayList<Tienda> listaTienda,CuentaBancaria cuentaBancaria) {
+        Fabrica.listaOperadores = listaOperadores;
+        Fabrica.listaProductos = listaProductos;
+        Fabrica.cantidadProducto = cantidadProducto;
+        Fabrica.listaTienda = listaTienda;
+        CuentaBancaria = cuentaBancaria;
     }
 
     //----------- Getters and Setters ----------------
@@ -142,7 +69,8 @@ public class Fabrica implements Financiero{
         CuentaBancaria = cuentaBancaria;
     }
 
-    //toString para saber que imprimir cuando se llame al objeto Fabrica        @Override
+    //toString para saber que imprimir cuando se llame al objeto Fabrica     
+    @Override   
     public String toString() {
         return "La mejor fabrica del mundo";
     }
@@ -151,45 +79,33 @@ public class Fabrica implements Financiero{
     // -------------- Metodos ------------------
     
     //muestra el catalogo en pantalla
-    public void mostrarProductos(){
-        int i = 1;
-        for (Producto producto : listaProductos) {
-            System.out.println(i+" "+producto.getNombre());
-            i++;
-        }
-    }
+    public String mostrarProductos(){
+		String textoProducto="";
+		int indice = 1;
+		//se recorre la lista para obtener cada nombre de las tiendas disponibles:
+		for(Producto producto:listaProductos) {
+			textoProducto += indice+". "+producto.getNombre()+"\n"; //se almacenan todos lo nombres de los productos en un string
+			indice++;
+		}
+		return textoProducto;
+	}
+    
     //muestra las tiendas en pantalla
-    public void mostrarTiendas(){
-        int i = 1;
-        for (Tienda tienda : listaTienda) {
-            System.out.println(i+" "+tienda.getNombre());
-            i++;
-        }
-    }
-    public void PagarOperario(Fabrica fabrica, Operario operario, double monto){
-        operario.recibirSueldo();
-    }
-    //Enviar productos a tienda:
-    //
-    public void Enviarproducto(Transporte transporte, Tienda tienda,Producto producto){
-        tienda.setListaCantidadProductos(map<producto,cantidadProducto>()); //aun no se muy bien como hacer esto xd
-    }
+	public String mostrarTiendas() {
+		String textoTiendas="";
+		int indice = 1;
+		//se recorre la lista para obtener cada nombre de las tiendas disponibles:
+		for(Tienda tienda:listaTienda) {
+			textoTiendas += indice+". "+tienda.getNombre()+"\n"; //se almacenan todos lo nombres de las tiendas en un string
+			indice++;
+		}
+		return textoTiendas;
+	}
+    // se selecciona la tienda en base a la opcion digitada por pantalla
+    public Tienda seleccionarTienda(int opcion) {
+		
+        Tienda tiendaSeleccionada = listaTienda.get(opcion);
 
-    public void Enviarproducto(Tienda tienda,Producto producto, int cantidad){
-       
-        /*
-         * 1. Se fija que el producto se va a enviar es uno de los que la tienda recibe en stock
-         * 
-         * 
-         */
-
-        if(tienda.getListaProductos().contains(producto)){ //1. Se fija que el producto se va a enviar es uno de los que la tienda recibe en stock
-         
-            tienda.getListaCantidadProductos() //El DICCIONARIO de productos se aumenta el valor en la cantidad especificada
-                .put(producto, tienda.getListaCantidadProductos().get(producto) + cantidad);
-
-        }else{
-            
-        }
-    }
+		return tiendaSeleccionada;
+	}
 }
--- src/uiMain/Distribuidora.java
+++ src/uiMain/Distribuidora.java
@@ -1,8 +1,80 @@
 package uiMain;
 import gestorAplicacion.gestion.*;
 public class Distribuidora {
+	static{
+		/* 
+		//NOTA: Toca arreglar los constructores y unas cosas pa ver donde va a ir eso 
+
+            //productos disponibles en la fabrica:
+
+            ArrayList<Producto> catalogo = new ArrayList<Producto>();
+            //Producto(String nombre, String descripcion, Double valor, Double peso, Double tamano, Double costoDeProduccion);
+            Producto producto1 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto2 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto3 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto4 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto5 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto6 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto7 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto8 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto9 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            Producto producto10 = new Producto("producto","descricion vacia", 10000, 5, 20,1);
+            //se agregan al catalogo
+            catalogo.add(producto1);
+            catalogo.add(producto2);
+            catalogo.add(producto3);
+            catalogo.add(producto4);
+            catalogo.add(producto5);
+        
+            //empleados de la fabrica:
+            
+            //cuentas para los empleados:
+            CuentaBancaria cuentaOperario1 = new CuentaBancaria(55555, 100000);
+            CuentaBancaria cuentaOperario2 = new CuentaBancaria(66666, 100000);
+            CuentaBancaria cuentaOperario3 = new CuentaBancaria(77777, 100000);
+        
+            ArrayList<Operario> nomina = new ArrayList<Operario>();
+            Operario operario1 = new Operario("operador1",20,55555,cuentaOperario1,Fabrica, 0);//cada oeprador tiene cien mil *hay que sacar cuentas*
+            Operario operario2 = new Operario("operador2",21,66666,cuentaOperario2,Fabrica, 0); //para ese atriubuto fabrica primero hay que crear una frabica para asignarselo
+            Operario operario3 = new Operario("operador3",22,77777,cuentaOperario3,Fabrica, 0);
+            //se agg a la nomina
+            nomina.add(operario1);
+            nomina.add(operario2);
+            nomina.add(operario3);
+        
+            //tiendas disponibles
+
+            //se crean los empleados para las tiendas: (uno para cada una)
+            //cuentas para los empleados de la tienda:
+            CuentaBancaria cuentaVendedor1 = new CuentaBancaria(55555, 100000); //cada vendedor tiene cien mil *hay que sacar cuentas*
+            CuentaBancaria cuentaVendedor2 = new CuentaBancaria(66666, 100000);
+            CuentaBancaria cuentaVendedor3 = new CuentaBancaria(77777, 100000);
+            //vendedores
+            Vendedor vendedor1 = new Vendedor("vendedor1",20,55555,cuentaVendedor1,Fabrica, 0);
+            Vendedor vendedor2 = new Vendedor("vendedor2",21,66666,cuentaVendedor2,Fabrica, 0);
+            Vendedor vendedor3 = new Vendedor("vendedor3",22,77777,cuentaVendedor3,Fabrica, 0);
+
+            //tiendas
+            ArrayList<Tienda> tiendas = new ArrayList<Tienda>();
+            Tienda tienda1 = new Tienda("tienda1",vendedor1,cuentaVendedor1); //eso atributo de la cuenta del vendedor hay que cambiarlo ya que 
+            Tienda tienda2 = new Tienda("tienda2",vendedor2,cuentaVendedor2); //cambiamos de opinion y ahora sera una solo cuenta para todas las tiendas.
+            Tienda tienda3 = new Tienda("tienda3",vendedor3,cuentaVendedor3);
+        
+            tiendas.add(tienda1);
+            tiendas.add(tienda2);
+            tiendas.add(tienda3);
+
+            //se le asigna la tienda a cada vendedor:
+            vendedor1.setTienda(tienda1);
+            vendedor2.setTienda(tienda2);
+            vendedor3.setTienda(tienda3);
+        //cuenta bancaria para la fabrica: // **la cuenta de la fabrica y de las tiendas deben ser las mismas **
+        CuentaBancaria cuentaFabrica = new CuentaBancaria(00000, 1000000); //saldo = un millon
+
+		*/   
+	}
 	public static void main(String[] args) {
 		
-
+	
 	}
 }
