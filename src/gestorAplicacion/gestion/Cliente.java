8c5a6afc8aa00c08b142f4a497b66494bdce7be8
MSRestreLeon <monicarestrepoleon@gmail.com>
2023-05-21 22:20:53 -0500

    Arreglo mostrarClientes <3
    Ya no saldrán duplicados

--- src/gestorAplicacion/gestion/Cliente.java
+++ src/gestorAplicacion/gestion/Cliente.java
@@ -112,10 +112,13 @@ public class Cliente implements Moda, Serializable{
 		int index = 1;
 		/*Se recorre la lista y se retornan todos los elementos pero solo como String*/
 		for(Cliente cliente:listaClientes) {
-			cadena +=index+". "+cliente+"\n"; /*Se hace uso del metodo toString asi devolvemos el objeto como tal*/
+			if (!cadena.contains(cliente.getNombre())){
+			cadena +=index+". "+ cliente +"\n"; /*Se hace uso del metodo toString asi devolvemos el objeto como tal*/
 			index++;
+			}
 		}
 		return cadena;
+		
 	}
 	
 
