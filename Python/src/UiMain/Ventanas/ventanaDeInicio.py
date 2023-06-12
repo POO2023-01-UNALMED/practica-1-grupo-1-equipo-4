709eff5a8d50dc9d739c2fdab49a47b9c05a1d35
Wakamole2002 <luvarelao@unal.edu.co>
2023-06-11 22:53:26 -0500

    ventana principal
    pinshe ventana principal
    
    Co-Authored-By: Joan <62775261+Extantword@users.noreply.github.com>
    Co-Authored-By: jcastaneda30 <119982731+jcastaneda30@users.noreply.github.com>
    Co-Authored-By: mariafernandacll2808 <124931004+mariafernandacll2808@users.noreply.github.com>
    Co-Authored-By: Mónica Restrepo L. <65642162+monicasofiarestrepo@users.noreply.github.com>

--- Python/src/UiMain/Ventanas/ventanaDeInicio.py
+++ Python/src/UiMain/Ventanas/ventanaDeInicio.py
@@ -17,9 +17,76 @@ class VentanaDeInicio(tk.Tk):
         # Configurar y mostrar los widgets iniciales en la ventana
 
         # Agregar un botón para restablecer la pantalla
-        reset_button = tk.Button(self, text="Ventana principal del administrador", command=self.abrir_ventana_principal)
-        reset_button.pack()
 
+
+
+        # --------------- APARTIR DE ACA LUIS HARÁ SU MAGIA PARA NO DAÑAR LO DEMAS --------------
+
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
+        # Crear el marco izquierdo
+        marco_izquierdo = tk.Frame(self, bg="#45ffb8")
+        marco_izquierdo.columnconfigure(0, weight=1)
+        marco_izquierdo.grid(row=0, column=0,padx=10, pady=10, sticky="nsew")
+        # Crear el marco derecho
+        marco_derecho = tk.Frame(self, bg="#4287f5")
+        marco_derecho.columnconfigure(0, weight=1)
+        marco_derecho.grid(row=0, column=1,padx=10, pady=10, sticky="nsew")
+        # Configurar el peso para que los marcos se ajusten al tamaño de la ventana
+        self.rowconfigure(0, weight=2)
+
+        #todo lo del lado izquierdo
+
+        marco_izquierdo.rowconfigure(0,pad=5, weight=1)
+        marco_izquierdo.rowconfigure(1,pad=5, weight=1)
+        marco_izquierdo.rowconfigure(2,pad=5, weight=1)
+
+        izquierdo_1 = tk.Frame(marco_izquierdo, bg="#70fa2a") #, width="400", height="100"
+        izquierdo_1.rowconfigure(0, weight=1)
+        izquierdo_1.columnconfigure(0, weight=1)
+        izquierdo_1.grid(row=0, padx=10, pady=10, sticky="nsew")
+        #aqui va lo que vaya dentro del frame
+        algo = tk.Label(izquierdo_1, text="ayuda esto no es un meme")
+        algo.grid(row=0, padx=20, pady=20, sticky="nsew")
+
+        izquierdo_2 = tk.Frame(marco_izquierdo, bg="#e8eb34") #, width="400", height="100"
+        izquierdo_2.grid(row=1, padx=10, pady=10, sticky="nsew")
+        izquierdo_2.rowconfigure(0, weight=1)
+        izquierdo_2.columnconfigure(0, weight=1)
+        algo2 = tk.Label(izquierdo_2, text="aqui deben ir algunas imagenes")
+        algo2.grid(row=0, padx=20, pady=20, sticky="nsew")
+
+
+        izquierdo_3 = tk.Frame(marco_izquierdo, bg="#eb8f34") #, width="400", height="100"
+        izquierdo_3.grid(row=2, padx=10, pady=10, sticky="nsew")
+        izquierdo_3.rowconfigure(0, weight=1)
+        izquierdo_3.columnconfigure(0, weight=1)
+        reset_button = tk.Button(izquierdo_3, text="Ventana principal del administrador", command=self.abrir_ventana_principal, height=10)
+        reset_button.grid(row=0, padx=20, pady=20, sticky="nsew")
+
+        #todo lo del lado derecho
+
+        marco_derecho.rowconfigure(0,pad=5, weight=1)
+        marco_derecho.rowconfigure(1,pad=5, weight=1)
+
+        derecho_1 = tk.Frame(marco_derecho, bg="#ba3cf0", width=200, height=100) #, width="400", height="100"
+        derecho_1.grid(row=0, padx=10, pady=10, sticky="nsew")
+        derecho_1.rowconfigure(0, weight=1)
+        derecho_1.columnconfigure(0, weight=1)
+        algo3 = tk.Label(derecho_1, text="ayuda esto no es un meme")
+        algo3.grid(row=0, padx=20, pady=20, sticky="nsew")
+
+        derecho_2 = tk.Frame(marco_derecho, bg="#f5425a") #, width="400", height="100"
+        derecho_2.grid(row=1, padx=10, pady=10, sticky="nsew")
+        derecho_2.rowconfigure(0, weight=1)
+        derecho_2.columnconfigure(0, weight=1)
+        algo4 = tk.Label(derecho_2, text="aqui deben ir algunas imagenes")
+        algo4.grid(row=0, padx=20, pady=20, sticky="nsew")
+
+        # ---------------      ACÁ TERMINA LA MAGIA DE LUIS             -------------
+
+        
     def reset_screen(self):
         # Destruir la ventana actual
         #Ubicacion serializidor
