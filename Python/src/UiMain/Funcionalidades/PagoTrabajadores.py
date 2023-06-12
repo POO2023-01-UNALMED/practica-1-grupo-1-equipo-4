c3e4a03b0d5cde9f0bdb13970517fc9dd207135a
mariafernandacll2808 <124931004+mariafernandacll2808@users.noreply.github.com>
2023-06-12 00:41:37 -0500

    Avance funcionalidad pago
    Co-Authored-By: jcastaneda30 <119982731+jcastaneda30@users.noreply.github.com>
    Co-Authored-By: Joan <62775261+Extantword@users.noreply.github.com>
    Co-Authored-By: Luis Varela <124922347+Wakamole2002@users.noreply.github.com>
    Co-Authored-By: Mónica Restrepo L. <65642162+monicasofiarestrepo@users.noreply.github.com>

--- Python/src/UiMain/Funcionalidades/PagoTrabajadores.py
+++ Python/src/UiMain/Funcionalidades/PagoTrabajadores.py
@@ -1,22 +1,38 @@
 import tkinter as tk
-from tkinter import ttk,Frame, END
-from tkinter import scrolledtext
+from tkinter import ttk,Frame
 
 class PagoTrabajadores(Frame):
     def __init__(self, window):
         super().__init__(window)
 
         #--------------Eventos --------------
-        def opcionTipoTrabajador():
+        def opcionTipoTrabajador(evento):
+            opc = desplegableTipos.get()
+
+            if opc == "Conductores":
+                frameTipos12.grid()
+                desplegableTrabajadores['values'] = ["Conductor 1", "Conductor 2", "Conductor 3"]
+            elif opc == "Operarios":
+                frameTipos12.grid()
+                desplegableTrabajadores['values'] = ["Operario 1", "Operario 2", "Operario 3"]
+            elif opc == "Vendedores":
+                frameTipos12.grid()
+                desplegableTrabajadores['values'] = ["Vendedor 1", "Vendedor 2", "Vendedor 3"]
+            else:
+                frameTipos12.grid_remove()
+                desplegableTrabajadores['values'] = ()
+
+        def opcionTrabajador(evento):
             pass
 
+        #--------------Divisiones filas y columnas --------------
         for i in range(8):
             self.rowconfigure(i, weight=1)
 
         for j in range(4):
             self.columnconfigure(j, weight=1)
 
-        #--------------Titulo y descripción --------------
+        #--------------Título y descripción --------------
         frameCabecera = tk.Frame(self)
         frameCabecera.grid(row=0, column=0, columnspan=4,padx=5, pady=5)
         titulo = tk.Label(frameCabecera, text='Pagar a trabajadores', font=("Arial", 15))
@@ -41,21 +57,27 @@ class PagoTrabajadores(Frame):
         tipoPredeterminado = tk.StringVar(value='Seleccionar tipo')
         desplegableTipos = ttk.Combobox(frameTipos11, values=["Operarios", "Conductores", "Vendedores"], textvariable=tipoPredeterminado, state='readonly')
         desplegableTipos.pack(side='top', anchor='center')
-        #desplegableTipos.bind("<<ComboboxSelected>>",opcionTipoTrabajador)
+        desplegableTipos.bind("<<ComboboxSelected>>", opcionTipoTrabajador)
 
         #--------------Trabajadores --------------
         frameTipos12 = tk.Frame(frameTipos1)
         frameTipos12.grid(row=1, column=2, padx=5, pady=5)
+        frameTipos12.grid_remove()
 
         textoTrabajadores = tk.Label(frameTipos12, text='Trabajadores', font=("Arial", 12))
         textoTrabajadores.pack(side='top', anchor='center')
 
-        trabajadorPredeterminado = tk.StringVar(value='Seleccionar tipo')
-        desplegableTrabajadores = ttk.Combobox(frameTipos12,values=["Roberto", "Carlitos", "Messi"],textvariable=trabajadorPredeterminado, state='readonly')
+        trabajadorPredeterminado = tk.StringVar(value='Seleccionar trabajador')
+        desplegableTrabajadores = ttk.Combobox(frameTipos12, textvariable=trabajadorPredeterminado, state='readonly')  # Cambio aquí
         desplegableTrabajadores.pack(side='top', anchor='center')
-        #desplegableTrabajadores.bind("<<ComboboxSelected>>",opcionTrabajador)
+        desplegableTrabajadores.bind("<<ComboboxSelected>>",opcionTrabajador)
 
+        #--------------Información trabajador seleccionado --------------
 
+        #--------------Bonificación metas --------------
+
+        #--------------Pago --------------
+        
 
 
 
