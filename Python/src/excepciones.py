429b04a7089ec63291ee54487f10f97659c5f30e
mariafernandacll2808 <124931004+mariafernandacll2808@users.noreply.github.com>
2023-06-15 22:11:54 -0500

    Create excepciones.py
    Co-Authored-By: Luis Varela <124922347+Wakamole2002@users.noreply.github.com>
    Co-Authored-By: Joan <62775261+Extantword@users.noreply.github.com>
    Co-Authored-By: Mónica Restrepo L. <65642162+monicasofiarestrepo@users.noreply.github.com>

---
+++ Python/src/excepciones.py
@@ -0,0 +1,45 @@
+
+class ErrorAplicacion(Exception):
+    def __init__(self, msg):
+        self._excepcion = "Manejo de errores de la Aplicacion: " + msg
+        super().__init__(self._excepcion)
+
+    def mostrarMensaje(self):
+        print(self._excepcion)
+
+class ExceptionC1(ErrorAplicacion):
+    def __init__(self, msg):
+        self._excepcion = "ExceptionC1" + msg
+        super().__init__(self._excepcion)
+
+class ExceptionC2(ErrorAplicacion):
+    def __init__(self, msg):
+        self._excepcion = "ExceptionC2" + msg
+        super().__init__(self._excepcion)
+
+#Primera rama
+class ExceptionInventada1(ExceptionC1):
+    def __init__(self):
+        super().__init__("ExceptionInventada1")
+
+class ExceptionInventada2(ExceptionC1):
+    def __init__(self):
+        super().__init__("ExceptionInventada2")
+
+class ExceptionSugerida1(ExceptionC1):
+    def __init__(self):
+        super().__init__("ExceptionSugerida1")
+
+
+#Segunda rama
+class ExceptionInventada3(ExceptionC2):
+    def __init__(self):
+        super().__init__("ExceptionInventada3")
+
+class ExceptionInventada4(ExceptionC2):
+    def __init__(self):
+        super().__init__("ExceptionInventada4")
+
+class ExceptionSugerida2(ExceptionC2):
+    def __init__(self):
+        super().__init__("ExceptionSugerida2")
